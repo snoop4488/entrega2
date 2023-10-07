9a99e66af8d206804844c604b2118d34fa2fd69c
snoop4488 <143089036+snoop4488@users.noreply.github.com>
2023-10-07 14:43:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,19 @@
 import streamlit as st
-import cv2
-import numpy as np
-#from PIL import Image
-from PIL import Image as Image, ImageOps as ImagOps
-from keras.models import load_model
+from bokeh.models.widgets import Button
+from bokeh.models import CustomJS
 
-model = load_model('keras_model.h5')
-data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+text = st.text_input("Que Hablar")
 
-st.title("Reconocimiento de Imágenes")
+tts_button = Button(label="Decirlo", width=100)
 
-img_file_buffer = st.camera_input("Toma una Foto")
+tts_button.js_on_event("button_click", CustomJS(code=f"""
+    var u = new SpeechSynthesisUtterance();
+    u.text = "{text}";
+    u.lang = 'en-US';
 
-if img_file_buffer is not None:
-    # To read image file buffer with OpenCV:
-    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-   #To read image file buffer as a PIL Image:
-    img = Image.open(img_file_buffer)
+    speechSynthesis.speak(u);
+    """))
+    
+st.bokeh_chart(tts_button,use_container_width=True)
 
-    newsize = (224, 224)
-    img = img.resize(newsize)
-    # To convert PIL Image to numpy array:
-    img_array = np.array(img)
-
-    # Normalize the image
-    normalized_image_array = (img_array.astype(np.float32) / 127.0) - 1
-    # Load the image into the array
-    data[0] = normalized_image_array
-
-    # run the inference
-    prediction = model.predict(data)
-    print(prediction)
-    if prediction[0][0]>0.5:
-      st.header('Izquierda, con Probabilidad: '+str( prediction[0][0]) )
-    if prediction[0][1]>0.5:
-      st.header('Arriba, con Probabilidad: '+str( prediction[0][1]))
-    if prediction[0][2]>0.5:
-      st.header('Derecha, con Probabilidad: '+str( prediction[0][2]))
+    
